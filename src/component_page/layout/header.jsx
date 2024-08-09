c4538945071bb0227ea4d632d40a8b163aef46f9
phatdeptraiii <vitphat350@gmail.com>
2024-08-09 15:11:33 +0700

    .

--- src/component_page/layout/header.jsx
+++ src/component_page/layout/header.jsx
@@ -1,37 +1,67 @@
-import React, { useState } from 'react';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  PhoneFilled,
+  PhoneOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Divider, Menu } from "antd";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    const items = [
+  const items = [
+    {
+      label: <Link to={"/"}>Home</Link>,
+      // label: 'Home Page',
+      key: "home",
+    },
+    {
+      label: "Sign in",
+      key: "SubMenu",
+      icon: <SettingOutlined />,
+      children: [
         {
-            label: <Link to={"/"}>Home Page</Link>,
-            // label: 'Home Page',
-            key: 'home',
-            icon: <MailOutlined />,
+          label: <Link to={"login"}>Login</Link>,
+          key: "login",
         },
         {
-            label: 'Sign in',
-            key: 'SubMenu',
-            icon: <SettingOutlined />,
-            children: [
-                {
-                    label: <Link to={"login"}>Login</Link>,
-                    key: 'login',
-                },
-                {
-                    label: <Link to={"register"}>Register</Link>,
-                    key: 'register'
-                },
-            ],
+          label: <Link to={"register"}>Register</Link>,
+          key: "register",
         },
-    ];
-    const [current, setCurrent] = useState('mail');
-    const onClick = (e) => {
-        console.log('click ', e);
-        setCurrent(e.key);
-    };
-    return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+      ],
+    },
+  ];
+  const [current, setCurrent] = useState("mail");
+  const onClick = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+  return (
+    <>
+      <header
+        className="header flex flex-row justify-between items-center p-10 bg-danger"
+        // style={{ backgroundColor: "rgba(15, 32, 48, 1)" }}
+      >
+        <div className="logo basis-1/4">
+          <img
+            src="./src/assets/image/image 1.png"
+            alt="Logo"
+            className="h-10 w-10"
+          />
+        </div>
+        <p className="bg-danger text-blue">ádljádnakl</p>
+        <div className="contact basis-3/4 flex justify-end items-center  text-white">
+          <PhoneOutlined className="text-lg" />
+        </div>
+      </header>
+      <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+      />
+    </>
+  );
 };
 export default Header;
