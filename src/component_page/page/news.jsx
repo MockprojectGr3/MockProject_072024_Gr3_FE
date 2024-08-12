75fd92624989b6caaa3bab458555f33b13821ab9
Dang Huynh <danghuynh1526@gmail.com>
2024-08-13 00:03:19 +0700

    feat/news-page_DNHuynh

--- src/component_page/page/news.jsx
+++ src/component_page/page/news.jsx
@@ -8,60 +8,64 @@ import '../../style/newspage.css';
 import Footer from '../layout/Footer.jsx';
 import Header from '../layout/header.jsx';
 const NewsPage = () => {
-    // const data= [
-    //     {
-    //         id: 1,
-    //         title: ''
-    //     }
-    // ]
+    const data = [
+        {
+            id: 1,
+            title: 'John Lewis to make final journey across Edmund Pettus Bridge in procession',
+            content: 'The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.',
+            img_path: img1
+        },
+        {
+            id: 2,
+            title: 'John Lewis, civil rights giant, crosses infamous Selma bridge one final time',
+            content: 'Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge',
+            img_path: img2
+        },
+        {
+            id: 3,
+            title: 'John Lewis, civil rights giant, crosses infamous Selma bridge one final time',
+            content: 'Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge',
+            img_path: img3
+        },
+        {
+            id: 4,
+            title: 'Tornado and tide warnings as Storm Hanna lashes Texas',
+            content: 'The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.',
+            img_path: img4
+        },
+        {
+            id: 5,
+            title: '20 Years Ago, Steve Jobs Built the ‘Coolest Computer Ever.’ It Bombed',
+            content: 'This month marks the 20th anniversary of the Power Mac G4 Cube, which debuted July 19, 2000. It also marks the 19th anniversary of Apple’s announcement that it was putting the Cube on ice.',
+            img_path: img5
+        },
+        {
+            id: 6,
+            title: '20 Years Ago, Steve Jobs Built the ‘Coolest Computer Ever.’ It Bombed',
+            content: 'This month marks the 20th anniversary of the Power Mac G4 Cube, which debuted July 19, 2000. It also marks the 19th anniversary of Apple’s announcement that it was putting the Cube on ice.',
+            img_path: img6
+        }
+    ]
     return (
         <>
             <Header />
             <h1 style={{ textAlign: 'center', margin: '30px 0px', fontSize: '30px' }}>NEWS</h1>
             <div className="card-container">
-
-                <div className="card">
-                    <img src={img1} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className='card-title'>John Lewis to make final journey across Edmund Pettus Bridge in procession</h5>
-                        <p className="card-text">The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src={img2} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className='card-title'>John Lewis to make final journey across Edmund Pettus Bridge in procession</h5>
-                        <p className="card-text">The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src={img3} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className='card-title'>John Lewis to make final journey across Edmund Pettus Bridge in procession</h5>
-                        <p className="card-text">The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src={img4} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className='card-title'>John Lewis to make final journey across Edmund Pettus Bridge in procession</h5>
-                        <p className="card-text">The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src={img5} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className='card-title'>John Lewis to make final journey across Edmund Pettus Bridge in procession</h5>
-                        <p className="card-text">The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</p>
-                    </div>
-                </div>
-                <div className="card">
-                    <img src={img6} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className='card-title'>John Lewis to make final journey across Edmund Pettus Bridge in procession</h5>
-                        <p className="card-text">The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</p>
-                    </div>
-                </div>
+                {
+                    data.map((item, index) => {
+                        return (
+                            <div className="card"
+                                key={index}
+                            >
+                                <img src={item.img_path} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className='card-title'>{item.title}</h5>
+                                    <p className="card-text">{item.content}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
             <Footer />
         </>
