be60d6b9d257b5f20366d45174ff7de360877813
ThuyNgan015 <145478844+ThuyNgan015@users.noreply.github.com>
2024-08-16 08:17:30 +0700

    feat/training-detail_HTNgan (#26)

--- src/router_page.jsx
+++ src/router_page.jsx
@@ -23,6 +23,7 @@ import News_DeTail_Page from './component_page/page/news_detail.jsx'
 import FeedbackPage from './component_page/page/feedback.jsx'
 import Service_Detail_Page from './component_page/page/service_detail.jsx'
 import Training from './component_page/page/training.jsx'
+import TrainingDetail from './component_page/page/training_detail.jsx'
 import FAQ_Page from './component_page/page/faq.jsx'
 import Equipment_Page from './component_page/page/equipment.jsx'
 
@@ -90,6 +91,8 @@ const router = createBrowserRouter([
         element: <Training />
       },
       {
+        path: "training-detail",
+        element: <TrainingDetail />
         path: "service_detail",
         element: <Service_Detail_Page />
       },
