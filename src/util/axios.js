d6c04c9ded76b36752657cb386ec581e0e3be6c3
Tran Duc Phat <144576717+tranducphat204@users.noreply.github.com>
2024-08-21 09:58:18 +0700

    Feat/handle login api d phat (#41)
    * .
    
    * feat/handle-register-API_DPhat_Done
    
    * feat/handle-login-API_DPhat_Done

--- src/util/axios.js
+++ src/util/axios.js
@@ -56,7 +56,7 @@ import axios from "axios";
 
 const servers = {
   php: "dec4-14-176-232-65",
-  node: "f9e8-116-105-20-219",
+  node: "7e29-14-176-232-65",
   java: "8c76-171-243-49-233",
   python: "8c76-171-243-49-233",
 };
