79464835bac4c5f57120713fed4fc2944af34362
phatdeptraiii <vitphat350@gmail.com>
2024-08-21 09:05:13 +0700

    feat/merge-about-us_DPhat_Done

--- src/component_page/page/aboutUs.jsx
+++ src/component_page/page/aboutUs.jsx
@@ -1,7 +1,31 @@
-function ABoutUs(){
-    return(
-        <div></div>
-    )
+import AboutGuard from "../AboutGuard";
+import Banner from "./banner";
+import OurService from "./OurService";
+import ExpertsSecurity from "./ExpertsSecurity";
+import ProfessionalTeam from "./ProfessionalTeam";
+import SecurityPage from "./SecurityPage";
+
+function ABoutUs() {
+  return (
+    <div>
+      <Banner />
+      <div className="mx-24">
+        <AboutGuard />
+      </div>
+      <div className="mx-24">
+        <OurService />
+      </div>
+      <div className="mx-24">
+        <ExpertsSecurity />
+      </div>
+      <div className="mx-24">
+        <ProfessionalTeam />
+      </div>
+      <div className="mx-24">
+        <SecurityPage />
+      </div>
+    </div>
+  );
 }
 
 export default ABoutUs;
