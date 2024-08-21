2ccbb59d197bd560ed9e884b31dd103299ac152c
phatdeptraiii <vitphat350@gmail.com>
2024-08-21 16:40:57 +0700

    feat/handle-our-service-API_DPhat_Done

--- src/component_page/page/OurService.jsx
+++ src/component_page/page/OurService.jsx
@@ -1,27 +1,26 @@
+import { useEffect, useState } from "react";
+import request from "../../util/axios";
+
 function OurService() {
-  const mockData = [
-    {
-      id: 1,
-      title: "Risk Management",
-      description:
-        "In crisis moments, our response to attacks is swift and strategic, leveraging the expertise of our trustworthy professionals. We stand as a reliable force, committed to neutralizing threats and safeguarding your security with integrity.",
-      image: "./src/assets/image/ser1.png",
-    },
-    {
-      id: 2,
-      title: "Crisis Management",
-      description:
-        "In crisis moments, our response to attacks is swift and strategic, leveraging the expertise of our trustworthy professionals. We stand as a reliable force, committed to neutralizing threats and safeguarding your security with integrity..",
-      image: "./src/assets/image/ser2.png",
-    },
-    {
-      id: 3,
-      title: "Security Management",
-      description:
-        "In crisis moments, our response to attacks is swift and strategic, leveraging the expertise of our trustworthy professionals. We stand as a reliable force, committed to neutralizing threats and safeguarding your security with integrity..",
-      image: "./src/assets/image/ser3.png",
-    },
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      request({
+        method: "get",
+        serverType: "node",
+        apiEndpoint: "our-service",
+        onSuccess: (data) => {
+          setData(data);
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      });
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <center>
@@ -33,17 +32,17 @@ function OurService() {
         </div>
         <h2 style={{ fontSize: 36 }}>OUR COMPREHENSIVE SECURITY SERVICES</h2>
         <div className="flex flex-row flex-wrap justify-between mt-4">
-          {mockData.map((item) => (
+          {data.map((item) => (
             <div
               key={item.id}
               className="items-center bg-black text-white text-left"
               style={{ height: 460, width: 366 }}
             >
               <div className="p-4">
-                <h3 style={{ fontSize: 24, marginBottom: 14 }}>{item.title}</h3>
+                <h3 style={{ fontSize: 24, marginBottom: 14 }}>{item.name}</h3>
                 <p style={{ fontSize: 15 }}>{item.description}</p>
               </div>
-              <img src={item.image} alt={item.title} width="366" height="237" />
+              <img src={item.url} alt={item.name} width="366" height="237" />
             </div>
           ))}
         </div>
